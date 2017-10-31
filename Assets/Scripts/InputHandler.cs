12c377a0e3c9887c036e8895234a20592746fdd3
Keano Porcaro <keano.porcaro@gmail.com>
2017-10-31 13:13:58 +1030

    AR Project Template

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public Transform m_itemHolder;
+    public Transform m_sceneSpace;
+    public float m_DoubleClickTime = 0.3f;
+    private int m_NumClick = 0;
+    private float m_TimePassed = 0f;
+    private Vector2 m_MouseDownPosition;
+    private Vector2 m_MouseUpPosition;
+    private float m_LastMouseUpTime;
+
+    bool m_itemPicked = false;
+
+    void Update()
+    {
+        CheckInput();
+    }
+    private void CheckInput()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_MouseDownPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            m_NumClick++;
+            m_LastMouseUpTime = Time.time;
+        }
+
+        if (m_NumClick > 0)
+        {
+            if (m_TimePassed > m_DoubleClickTime) //If the max duration for a double click has passed then it's a single click
+            {
+                HandleClick();
+                m_TimePassed = 0;
+                m_NumClick = 0;
+            }
+            else
+            {
+                if (m_NumClick > 1) //Multiple click can be handled here but we just classify all of them as double click
+                {
+                    //Handle 2 or more clicks
+                    m_TimePassed = 0;
+                    m_NumClick = 0;
+                }
+            }
+            m_TimePassed += Time.deltaTime; //Keep track of time passed after a click
+                                            //Debug.Log(m_TimePassed);
+        }
+    }
+
+    void HandleClick()
+    {
+        Vector3 position = Input.mousePosition;
+        Ray inputRay = Camera.main.ScreenPointToRay(position);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit))
+        {
+            GameObject gameObj = hit.transform.gameObject;
+            if (gameObj.tag == "ShapeTemplate")
+                HandleShapeTemplate(gameObj);
+            else if (gameObj.tag == "TextureTemplate")
+                HandleTextureTemplate(gameObj);
+            else if (gameObj.tag == "Interactive")
+                HandleInteractive(gameObj);
+        }
+    }
+
+    void HandleShapeTemplate(GameObject gameObj)
+    {
+        if (m_itemPicked) //destroy object in hand first
+        {
+            GameObject obj = m_itemHolder.GetChild(0).gameObject;
+            if (obj)
+                Destroy(obj);
+        }
+        GameObject newObj = (GameObject)Instantiate(gameObj);
+        newObj.transform.SetParent(m_itemHolder, false);
+        newObj.transform.rotation = gameObj.transform.rotation;
+        newObj.tag = "Interactive";
+        m_itemPicked = true;
+    }
+
+    void HandleTextureTemplate(GameObject gameObj)
+    {
+        if (m_itemPicked)
+        {
+            GameObject obj = m_itemHolder.GetChild(0).gameObject;
+            if (obj)
+                obj.GetComponent<MeshRenderer>().material = gameObj.GetComponent<MeshRenderer>().material;
+        }
+    }
+
+    void HandleInteractive(GameObject gameObj)
+    {
+        if (m_itemPicked) //picked up
+        {
+            gameObj.transform.SetParent(m_sceneSpace);
+            m_itemPicked = false;
+        }
+        else
+        {
+            gameObj.transform.SetParent(m_itemHolder);
+            m_itemPicked = true;
+        }
+    }
+
+    public void DeleteObject()
+    {
+        if (m_itemPicked)
+        {
+            GameObject obj = m_itemHolder.GetChild(0).gameObject;
+            if(obj)
+                Destroy(obj);
+            m_itemPicked = false;
+        }
+    }
+}
---
+++ Assets/Scripts/VBTabHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class VBTabHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    private GameObject m_shapes;
+    private GameObject m_textures;
+
+    void Start()
+    {
+        // Register with the virtual buttons TrackableBehaviour
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; ++i)
+        {
+            vbs[i].RegisterEventHandler(this);
+        }
+
+        m_shapes = transform.Find("Shapes").gameObject;
+        m_textures = transform.Find("Textures").gameObject;
+    }
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("OnButtonPressed: " + vb.VirtualButtonName);
+
+        switch (vb.VirtualButtonName)
+        {
+            case "Shape":
+                m_shapes.SetActive(true);
+                m_textures.SetActive(false);
+                break;
+
+            case "Texture":
+                m_shapes.SetActive(false);
+                m_textures.SetActive(true);
+                break;
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("OnButtonReleased");
+    }
+
+}
