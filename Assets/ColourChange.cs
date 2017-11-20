8ee77a9580bff69126b593618e28130f65e38526
Keano Porcaro <keano.porcaro@gmail.com>
2017-11-20 12:56:19 +1030

    TEST

---
+++ Assets/ColourChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChange : MonoBehaviour {
+
+    GameObject[] shapeList;
+
+    void Awake()
+    {
+        shapeList = GameObject.FindGameObjectsWithTag("ShapeTemplate");
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (GameObject shape in shapeList)
+        {
+            shape.GetComponent<MeshRenderer>().material = this.GetComponent<MeshRenderer>().material;
+        }
+    }
+}
